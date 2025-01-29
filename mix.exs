a82880735d21a437dff9fea9f25f75448234aa28
Ryan Shah <ryan.k.shah@gmail.com>
2025-01-29 20:17:55 +0000

    One more time...

--- mix.exs
+++ mix.exs
@@ -37,7 +37,7 @@ defmodule ExShark.MixProject do
       {:excoveralls, "~> 0.15", only: :test},
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.2", only: [:dev, :test], runtime: false},
-      {:sobelow, "~> 0.12", only: [:dev, :test], runtime: false},
+      {:sobelow, "~> 0.13", only: [:dev, :test], runtime: false},
       {:mix_audit, "~> 2.1", only: [:dev, :test], runtime: false}
     ]
   end
